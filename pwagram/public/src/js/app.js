3bd172a8ae7292c630e21332826e716caab9b58d
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-11-05 20:54:27 +0100

    service worker

---
+++ pwagram/public/src/js/app.js
@@ -0,0 +1,20 @@
+let deferredPrompt;
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(function () {
+    console.log("Service worker registered!");
+  });
+}
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log("beforeinstallprompt fired");
+
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  return false;
+
+  // Update UI notify the user they can install the PWA
+  // showInstallPromotion();
+});
