3d687495982a1b9faf347ae0d669945f73ced305
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-11-11 20:58:53 +0100

    cache api

--- pwagram/public/sw.js
+++ pwagram/public/sw.js
@@ -1,14 +1,21 @@
 self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Installing Service Worker...", event);
+  // console.log("[Service Worker] Installing Service Worker...", event);
+
+  event.waitUntil(
+    caches.open("static").then((cache) => {
+      console.log("[Service Worker] Precaching App Shell");
+      cache.add("/src/js/app.js");
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Activating Service Worker...", event);
+  // console.log("[Service Worker] Activating Service Worker...", event);
   return self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("[Service Worker] Fetching something...", event);
+  // console.log("[Service Worker] Fetching something...", event);
   event.respondWith(fetch(event.request));
 });
 
