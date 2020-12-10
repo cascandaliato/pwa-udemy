57876361ce70dafd959e4392c1f03f134334ae11
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-12-10 20:59:46 +0100

    minify code

--- pwagram/workbox-cli-config.js
+++ pwagram/workbox-cli-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   globDirectory: "public\\",
-  globPatterns: ["**/*.{html,ico,json,css,js}", "src/images/*.{jpg,png}"],
+  // globPatterns: ["**/*.{html,ico,json,css,js}", "src/images/*.{jpg,png}"],
+  globPatterns: [
+    "**/*.{html,ico,json,css}",
+    "src/images/*.{jpg,png}",
+    "srv/js/*.min.js",
+  ],
   swSrc: "public/service-worker-base.js",
   swDest: "public/service-worker.js",
   globIgnores: ["..\\workbox-cli-config.js", "help\\**"],
